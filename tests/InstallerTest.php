ad979e261684dd12fbc52a7e04df092ad1f9195a
Thom Williams <thomscode@gmail.com>
2017-11-09 14:05:27 -0800

    Add initial unit testing configuration

---
+++ tests/InstallerTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: will202
+ * Date: 11/7/17
+ * Time: 11:56 AM
+ */
+
+namespace pnnl\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class InstallerTest extends TestCase
+{
+
+    public function testInstaller()
+    {
+        $this->assertEquals(true, true);
+    }
+}
